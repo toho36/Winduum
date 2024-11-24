f0ed005973f0e6dcc9af191b9e9946497e673dbe
HoangVietTo <toho0036@gmail.com>
2024-11-24 13:34:52 +0100

    Initial commit

---
+++ eslint.config copy.js
@@ -0,0 +1,48 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:prettier/recommended',
+      prettierConfig,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error',
+      'import/no-default-export': 'error',
+      'eslint-comments/require-description': 'off',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+);
